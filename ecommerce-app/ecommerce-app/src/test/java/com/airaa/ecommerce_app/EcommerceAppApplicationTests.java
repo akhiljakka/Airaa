c4bba1fec77d25c6a3f6974e6529a498abf9fd5b
Akswi <akhil.jakka@gmail.com>
2024-09-13 22:47:01 +0200

    First commit

---
+++ ecommerce-app/ecommerce-app/src/test/java/com/airaa/ecommerce_app/EcommerceAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.airaa.ecommerce_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
